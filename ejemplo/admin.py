4cd337b04aa8f41819fc143a39b8cf237672b49c
Nockzb <132928494+Nockzb@users.noreply.github.com>
2024-02-05 23:20:18 +0100

    panel admin ok

--- ejemplo/admin.py
+++ ejemplo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Datos
+
 
 # Register your models here.
+@admin.register(Datos)
+class DatosAdmin(admin.ModelAdmin):
+    model = Datos
+    list_display = ["nombre", "apellido1", "apellido2"]
+    search_fields = ["nombre", "apellido1"]
+
--- ejemplo/models.py
+++ ejemplo/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Datos(models.Model): 
     id = models.IntegerField(primary_key=True) 
     nombre = models.CharField(max_length=50) 
-    apellido1 = models.CharField(max_length=50) 
-    apellido2 = models.CharField(max_length=50)
+    apellido1 = models.CharField(max_length=50, verbose_name="Primer apellido") 
+    apellido2 = models.CharField(max_length=50, verbose_name="Segundo apellido", blank=True, null=True)
+    
+    class Meta:        
+        verbose_name_plural = "Datos"
