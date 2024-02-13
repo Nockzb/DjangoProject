726f6aaab895e772a7ee53cf1cf43b5c2d81fb56
Nockzb <132928494+Nockzb@users.noreply.github.com>
2024-02-13 21:19:18 +0100

    avan

--- ejemplo/urls.py
+++ ejemplo/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name="ejemplo"
 urlpatterns = [
     path('', views.ejemplo),
-    path('usuario/', views.usuario),    
+    path('listado/', views.ListadoView.as_view(), name="listado"),
+    path('detalles/<int:pk>',views.DetalleView.as_view(), name="nombre"),      
 ]
--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from . import models
 from datetime import date
+from django.views import generic
 
 # Create your views here.
 def ejemplo(request):
@@ -10,20 +11,45 @@ def ejemplo(request):
     # segundo parametro es el path
     return render(request,"ejemplo.html",{"hoy":diferencia})
 
-def usuario(request):
-    datos = models.Datos.objects.all()   
-    usuario = datos[0]
+# def usuario(request):
+#     datos = models.Datos.objects.all()   
+#     usuario = datos[0]
     
-    context = {}    
-    context["id"] = usuario.id
-    context["nombre"] = usuario.nombre 
-    context["apellido1"] = usuario.apellido1
-    context["apellido2"] = usuario.apellido2
-    context["personas"] = datos # todos los datos de la BD
+#     context = {}    
+#     context["id"] = usuario.id
+#     context["nombre"] = usuario.nombre 
+#     context["apellido1"] = usuario.apellido1
+#     context["apellido2"] = usuario.apellido2
+#     context["personas"] = datos # todos los datos de la BD
     
-    context["listaVacia"] = [] # lista vacía para que se muestre la imágen
+#     context["listaVacia"] = [] # lista vacía para que se muestre la imágen
     
     
-    context["texto"] = "Prueba argumento"
-    return render(request, "usuario.html", context)
+#     context["texto"] = "Prueba argumento"
+#     return render(request, "usuario.html", context)
+
+class ListadoView(generic.ListView): 
+    model = models.Datos
+    template_name = "listado.html"
     
+class DetalleView(generic.DetailView): 
+    model = models.Datos 
+    template_name = "detalle.html"
+    
+class CreateView(generic.CreateView):
+    model = models.Datos
+    fields = ['nombre', 'apellido1', 'apellido2']
+    template_name = "crear.html" 
+    success_url = "/ejemplo/listado/" 
+
+class UpdateView(generic.UpdateView):
+    model = models.Datos
+    fields = ['nombre', 'apellido1', 'apellido2']
+    template_name = "actualizar.html" 
+    success_url = "/ejemplo/listado/"
+
+class DeleteView(generic.DeleteView):
+    model = models.Datos
+    fields = []
+    template_name = "eliminar.html" 
+    success_url = "/ejemplo/listado/"
